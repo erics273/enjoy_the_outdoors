26b8d8d15d1c4f04fd733a36eb70988a9876fb19
Eric Schwartz <eric@developintelligence.com>
2022-06-30 11:08:53 -0400

    demo day 4 display results

--- assets/scripts/national_parks.js
+++ assets/scripts/national_parks.js
@@ -10,6 +10,12 @@ let locationsDDL = document.querySelector("#locations");
 //get the locations dropdown so we can work with it
 let typesDDL = document.querySelector("#types");
 
+//pulling in the table stuff so i can work with it
+let searchResults = document.querySelector("#searchResults")
+let searchResultsBody = document.querySelector("#searchResults tbody")
+
+let resultCardsDiv = document.querySelector("#resultsCards")
+
 searchTypeDDL.addEventListener("change", function(event){
 
     //check which search type
@@ -25,20 +31,82 @@ searchTypeDDL.addEventListener("change", function(event){
     if(event.target.value === "location"){
         generateLocationsDDLOptions();
         locationsDDL.classList.remove("d-none")
-        typesDDL.classList.add("d-none")
     }
 
     if(event.target.value === "type"){
         generateTypesDDLOptions()
         typesDDL.classList.remove("d-none")
-        locationsDDL.classList.add("d-none")
     }
 
 })
 
 locationsDDL.addEventListener("change", function(event){
-    alert(event.target.value)
+
+    searchResults.classList.add("d-none")
+   
+    let location = event.target.value;
+
+    let filteredParks = nationalParksArray.filter((nationalPark)=>{
+        return nationalPark.State === location
+    })
+
+    console.log(filteredParks)
+
+    generateTableRows(filteredParks)
+    // generateCards(filteredParks)
+
+    searchResults.classList.remove("d-none")
+
 })
+
+
+function generateCards(someArrayOfData){
+
+    //clear out the tables previous results
+    resultCardsDiv.innerHTML = ""
+
+
+    //generate new table rows and appewnd to the the tbody innerHTML
+    someArrayOfData.forEach((park)=>{
+        let card = '<div class="col">'
+        card += '   <div class="card h-100">'
+        card += '       <img src="..." class="card-img-top" alt="...">'
+        card += '       <div class="card-body">'
+        card += '           <h5 class="card-title">'+ park.LocationName +'</h5>'
+        card += '           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>'
+        card += '       <div class="card-body">'
+        card += '           <h5 class="card-title">Card title</h5>'
+        card += '           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>'
+        card += '       </div>'
+        card += '   </div>'
+        card += '  </div>'
+        
+
+        resultCardsDiv.innerHTML += card
+    })
+
+
+}
+
+function generateTableRows(someArrayOfData){
+
+    //clear out the tables previous results
+    searchResultsBody.innerHTML = ""
+
+
+    //generate new table rows and appewnd to the the tbody innerHTML
+    someArrayOfData.forEach((park)=>{
+        let row = ""
+        row += `<tr>`
+        row += `    <td>${park.LocationName}</td>`
+        row += `    <td>${park.State}</td>`
+        row += `    <td>${park.Address}</td>`
+        row += `</tr>`
+
+        searchResultsBody.innerHTML += row
+    })
+
+}
 
 function generateLocationsDDLOptions(){
 
